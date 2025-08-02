e8ab9eb06eaa7238fedfac2b36695be55977f092
Jake Schuler <jschuler@hackclub.app>
2025-08-02 17:04:12 -0500

    turned off autocompletion on some forms

--- handlers/home.go
+++ handlers/home.go
@@ -1,35 +1,35 @@
 package handlers
 
 import (
-    "github.com/gin-gonic/gin"
-    "gorm.io/gorm"
-    
-    "github.com/Jake-Schuler/ORC-MatchMaker/services"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	"github.com/Jake-Schuler/ORC-MatchMaker/services"
 )
 
 var isSchedulePublic = false
 
 func HomeHandler(db *gorm.DB) gin.HandlerFunc {
-    return func(c *gin.Context) {
-        if !isSchedulePublic {
-            c.HTML(200, "index.tmpl", gin.H{
-                "title":            "ORC Match Maker",
-                "isSchedulePublic": isSchedulePublic,
-            })
-        } else {
-            c.HTML(200, "index.tmpl", gin.H{
-                "title":            "ORC Match Maker",
-                "matches":          services.ParseMatchSchedule(),
-                "isSchedulePublic": isSchedulePublic,
-            })
-        }
-    }
+	return func(c *gin.Context) {
+		if !isSchedulePublic {
+			c.HTML(200, "index.tmpl", gin.H{
+				"title":            "ORC Dashboard",
+				"isSchedulePublic": isSchedulePublic,
+			})
+		} else {
+			c.HTML(200, "index.tmpl", gin.H{
+				"title":            "ORC Dashboard",
+				"matches":          services.ParseMatchSchedule(),
+				"isSchedulePublic": isSchedulePublic,
+			})
+		}
+	}
 }
 
 func GetSchedulePublic() bool {
-    return isSchedulePublic
+	return isSchedulePublic
 }
 
 func SetSchedulePublic(value bool) {
-    isSchedulePublic = value
+	isSchedulePublic = value
 }
